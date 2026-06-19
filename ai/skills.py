7235bf9eb4df238c2edf5a83ab3455699fc20939
DurgePratik <iitianpratik2022@gmail.com>
2026-06-19 22:30:34 +0530

    Fix skills.csv path

--- ai/skills.py
+++ ai/skills.py
@@ -1,11 +1,22 @@
 import pandas as pd
+import os
+
+BASE_DIR = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
+skills_path = os.path.join(
+    BASE_DIR,
+    "data",
+    "skills.csv"
+)
 
 skills_df = pd.read_csv(
-    "data/skills.csv",
+    skills_path,
     header=None
 )
 
 SKILLS = [
-    skill.strip().lower()
+    str(skill).strip().lower()
     for skill in skills_df[0].tolist()
 ]
